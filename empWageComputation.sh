364a79bf9d568a9da889bd43d2488b03f9f3709e
bhavana <bhavanapoudala9@gmail.com>
2021-02-27 15:17:26 +0530

    Added: Part Time Employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-empCheck=$((RANDOM%2))
-isPresent=1
+empCheck=$((RANDOM%3))
+isFullTime=1
+isPartTime=2
 Wage_per_hr=20
 No_of_Hrs=8
-if [ $empCheck -eq $isPresent ]
+partTime_hr=8
+partTime_wage=10
+if [ $isFullTime -eq $empCheck ]
 then
-   Daily_wage=$(( $No_of_Hrs*$Wage_per_hr ))
-   echo "Daily wage is: " $Daily_wage
+	Daily_wage=$(( $Wage_per_hr*$No_of_Hrs ))
+	echo "Full time wage: " $Daily_wage
+
+elif [ $isPartTime -eq $empCheck ]
+then 
+   Daily_wage=$(( $partTime_hr*$partTime_wage ))
+   echo "Part time wage is: " $Daily_wage
 else
    Daily_wage=0
    echo $Daily_wage
