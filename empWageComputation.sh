dda5b0c25536dd6912b23d17c5696a2984135fda
bhavana <bhavanapoudala9@gmail.com>
2021-02-27 15:58:48 +0530

    Added: Employee wage during a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-empCheck=$((RANDOM%3))
+
 isFullTime=1
 isPartTime=2
+totalSalary=0
 Wage_per_hr=20
+Num_of_working_days=30
+for (( day=1; day<=$Num_of_working_days; day++ ))
+do
+   empCheck=$((RANDOM%3));
+	case $empCheck in
+		$isFullTime)
+			empHrs=8
+			;;
+		$isPartTime)
+			empHrs=4
+			;;
+		*)
+		empHrs=0
+			;;
+	esac
 
-case $empCheck in
-	$isFullTime)
-		empHrs=8
-		;;
-	$isPartTime)
-		empHrs=4
-		;;
-	*)
-	empHrs=0
-		;;
-esac
+	Daily_wage=$(( $Wage_per_hr*$empHrs ))
+	totalSalary=$(($Daily_wage+$totalSalary))
 
-Daily_wage=$(( $Wage_per_hr*$empHrs ))
-echo "Daily wage of an employee is: " $Daily_wage
+done
+echo "Total salary of an Employee in a month: " $totalSalary
+
 
 
 
