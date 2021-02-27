a4ae150d4b8890af2b07a834f01134c37b037236
bhavana <bhavanapoudala9@gmail.com>
2021-02-27 15:43:27 +0530

    Added: Daily wage of an Employee using Switch Case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,22 +4,22 @@ empCheck=$((RANDOM%3))
 isFullTime=1
 isPartTime=2
 Wage_per_hr=20
-No_of_Hrs=8
-partTime_hr=8
-partTime_wage=10
-if [ $isFullTime -eq $empCheck ]
-then
-	Daily_wage=$(( $Wage_per_hr*$No_of_Hrs ))
-	echo "Full time wage: " $Daily_wage
 
-elif [ $isPartTime -eq $empCheck ]
-then 
-   Daily_wage=$(( $partTime_hr*$partTime_wage ))
-   echo "Part time wage is: " $Daily_wage
-else
-   Daily_wage=0
-   echo $Daily_wage
-fi
+case $empCheck in
+	$isFullTime)
+		empHrs=8
+		;;
+	$isPartTime)
+		empHrs=4
+		;;
+	*)
+	empHrs=0
+		;;
+esac
+
+Daily_wage=$(( $Wage_per_hr*$empHrs ))
+echo "Daily wage of an employee is: " $Daily_wage
+
 
 
 
